81ba2325f4e9db5435f1722fab920cb62d568ea3
MayVladislav <56929850+MayVladislav@users.noreply.github.com>
2020-12-15 00:26:47 +0300

    Add files via upload

---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,42 @@
+﻿                                                                                                                                                                                                using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Shar s1, s2;
+#if DEBUG
+            s2 = new Shar(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(s2.ToString());
+#endif
+#if !DEBUG
+            s1 =Shar.CreatefromtheFile();
+            Console.WriteLine(s1.ToString());
+#endif
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+            Console.SetIn(save_in);
+            new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
---
+++ lab7/lab7/Shar.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    class Shar
+    {
+
+
+        public Shar() {}
+        public Shar (double pR)
+        {
+            R = pR;
+        }
+        public static Shar CreatefromtheFile()
+        {
+            double RR = Convert.ToDouble(Console.ReadLine());
+            return new Shar(RR);
+        }
+        private double R;
+       
+        public double Load(double R)
+        {
+            return R;
+        }
+        public void Load()
+        {
+            R = Convert.ToDouble(Console.ReadLine());
+        }
+
+        private double Diam()
+        {
+            return 2 * R;
+        }
+        private double Volume()
+        {
+            return 1.0f * 4 / 3 * Math.PI * Math.Pow(R, 3);
+        }
+        private double Square()
+        {
+            return 4 * Math.PI * Math.Pow(R, 2);
+        }
+       
+        public override string ToString()
+        {
+           
+
+            return $"Объем шара = {Volume()}\nПлощадь шара = {Square()}\nДиаметр шара = {Diam()}";
+        }
+
+    }
+}
