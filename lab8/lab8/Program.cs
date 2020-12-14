6ae2a4831975ac3ee80bc3f493b21615e3ea2a6b
MayVladislav <56929850+MayVladislav@users.noreply.github.com>
2020-12-15 00:26:57 +0300

    Add files via upload

---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Специалист[] mas = new Специалист[4];
+            mas[0] = new Программист("Иванов", "Иван", "Петрович", ЯзыкиПрограммирования.Java);
+            mas[1] = new Врач("Сергеев", "Петр", "Алексеевич", Специальности.Травматолог);
+            mas[2] = new Летчик("Дмитриев", "Евгений", "Петрович", Звания.Капитан);
+            mas[3] = new Специалист("Дмитриев", "Евгений", "Петрович");
+            
+
+            for (int i = 0; i<mas.Length; i++)
+            {
+                Console.WriteLine(mas[i].GetType().Name);
+                Console.WriteLine(mas[i].ФИО);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab8/lab8/Специалист.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    class Специалист
+    {
+        public Специалист(string Ф, string И, string О)
+        {
+            Фамилия = Ф;
+            Имя = И;
+            Отчество = О;
+
+        }
+
+
+
+        string Фамилия, Имя, Отчество;
+
+        public virtual string ФИО
+        {
+            get 
+            {
+                return Фамилия + " " + Имя + " " + Отчество;
+            }
+        }
+    }
+    class Программист:Специалист
+    {
+        public Программист(string Ф, string И, string О, ЯзыкиПрограммирования языкиП)
+            : base(Ф,И,О)
+        {
+            ЯзыкПрограммирования = языкиП;
+        }
+        public ЯзыкиПрограммирования ЯзыкПрограммирования;
+
+
+        public override string ФИО
+        {
+            get
+            {
+                return base.ФИО + " " + ЯзыкПрограммирования.ToString();
+            }
+        }
+    }
+
+    class Врач : Специалист
+    {
+        public Врач(string Ф, string И, string О, Специальности специальности)
+            : base(Ф, И, О)
+        {
+            Специальность = специальности;
+        }
+        public Специальности Специальность;
+
+
+        public override string ФИО
+        {
+            get
+            {
+                return base.ФИО + " " + Специальность.ToString();
+            }
+        }
+    }
+
+    class Летчик : Специалист
+    {
+        public Летчик(string Ф, string И, string О, Звания звания)
+            : base(Ф, И, О)
+        {
+            Звание = звания;
+        }
+        public Звания Звание;
+
+
+
+        public override string ФИО
+        {
+            get
+            {
+                return  Звание.ToString() + " "+ base.ФИО ;
+            }
+        }
+    }
+    public enum ЯзыкиПрограммирования 
+    {
+        Python,
+        Pascal,
+        Java,
+        Kotlin
+
+
+    }
+    public enum Звания
+    {
+        Полковник,
+        Майор,
+        Лейтенант,
+        Капитан
+    }
+    public enum Специальности
+    {
+        Терапевт,
+        Хирург,
+        Травматолог,
+        ЛОР
+    }
+}
