342dbf1836fa38409df039f2ad4af5b7e854b5d6
MayVladislav <56929850+MayVladislav@users.noreply.github.com>
2020-12-15 00:25:15 +0300

    Add files via upload

---
+++ lab6/lab6/Graf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Graf
+    {
+        private double a, b, sum, z;
+        public void Load()
+        {
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            sum = Math.Pow(b, 2) / 2 - Math.Pow(a, 2) / 2;
+            z = b - a;
+            return $"Плащадь равна: {sum.ToString()}\nРасстояние от а до b = {z.ToString()} ";
+        }
+    }
+}
---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG  
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+ #endif
+
+            Graf x = new Graf();
+
+            x.Load();
+            Console.WriteLine(x.ToString());
+
+ #if !DEBUG 
+            Console.SetOut(save_out);
+            new_out.Close();
+            Console.SetIn(save_in);
+            new_in.Close();
+ #else 
+            Console.ReadKey();
+ #endif
+        }
+        
+    }
+}
