526b869bda8e73f1209f7e2f8a27ec10688e3b8c
Vladislav <89187635177izi@gmail.com>
2020-12-20 16:23:15 +0300

    lab 11 update

---
+++ lab11_csv/lab11/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11
+{
+    class Person
+    {
+        public string ID { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Email { get; set; }
+        public double Plus { get; set; }
+        public double Minus { get; set; }
+        public long Credit_card { get; set; }
+        public double Tax { get; set; }
+
+
+       /* public override string ToString()
+        {
+            String s = string.Format(
+                "**************************\n" +
+                "ID:{0}, Имя: {1}, Фамилия: {2}, E-mail: {3}, \n"+
+                "Пол.Баланс: {4}, Отр.Баланс {5}, Налоги: {6}",
+                ID,Fname,Lname, TrueMail(Email), Plus,Minus,Credit_card,Tax);
+            return s;
+        }
+       */
+
+        public static Person Create(String line)
+        {
+            Person p = new Person();
+            String[] e = line.Split(',');
+            p.ID = e[0].Trim();
+            p.Fname = e[1].Trim();
+            p.Lname = e[2].Trim();
+            p.Email = e[3].Trim();
+            p.Plus = Convert.ToDouble(e[4].TrimStart('$').Replace('.',','));
+            p.Minus = Convert.ToDouble(e[5].TrimStart('$').Replace('.', ','));
+            p.Credit_card = Convert.ToInt64(e[6].Trim());
+            p.Tax = Convert.ToDouble(e[5].TrimStart('$').Replace('.', ','));
+
+            return p;
+        }
+        private static string TrueMail(string Email)
+                {
+            if (Email == "") 
+                return "Нет почты";
+            else 
+                return Email;
+                }
+    }       
+}
---
+++ lab11_csv/lab11/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader f_in = new StreamReader("lr11_21.csv");
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            StreamWriter new_out = new StreamWriter(@"lr_output.txt");
+            Console.SetOut(new_out);
+#endif
+
+            List<Person> all = new List<Person>();
+            
+            try
+            {
+                String line = f_in.ReadLine();
+                while((line = f_in.ReadLine()) != null)
+                {
+                    all.Add(Person.Create(line));
+                }
+                   
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Всего пользователей: {0}", all.Count);
+            /*foreach (var p in all)
+            {
+                Console.WriteLine(p);
+            }
+           */
+                
+            
+            Console.WriteLine("*********Задача 1************");
+
+            double counts = 0;
+            int k = 0;
+            for (int i = 0; i<all.Count; i++)
+            {
+                if (all[i].Plus - all[i].Minus > 0)
+                {
+                    k += 1;
+                    counts+= Convert.ToDouble(all[i].Plus -all[i].Minus);
+                }
+                
+            }
+            Console.WriteLine($"Средний баланс среди положительных счетов = {counts/k}\n");
+
+            Console.WriteLine("**********Задача 2**********");
+            Console.WriteLine($"Количество счетов с отрицательным балансом = {all.Count - k}\n");
+           
+                        Console.WriteLine("**********Задача 3**********");
+                        int lownalog = 0;
+                        for (int i = 0; i < all.Count; i++)
+                        {
+                            if (all[i].Plus -all[i].Minus < all[i].Tax)
+                            {
+                                lownalog++;
+                            }
+
+                        }
+                        Console.WriteLine($"Счетов, где баланса недостаточно для уплаты налага = {lownalog}\n");
+ 
+                        Console.WriteLine("**********Задача 4**********");
+                        double maxbalance = 0;
+                        string Name = "";
+
+                        for (int i = 0; i < all.Count; i++)
+                        {
+                            if (all[i].Plus - all[i].Minus > maxbalance)
+                            {
+                                maxbalance = all[i].Plus - all[i].Minus;
+                                Name = $"{all[i].Fname} {all[i].Lname}"; 
+                            }
+
+                        }
+                        Console.WriteLine($"Максимальный баланс = {maxbalance}, Имя владельца = {Name}");
+
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
